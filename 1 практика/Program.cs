22d2f6b14b2e3bc3cddf6a5ffeaf34f0ccf7efe9
Your Name <mitrofpnovkirill@gmail.com>
2021-04-23 09:54:13 +0300

    first commit

---
+++ 1 практика/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub1
+{
+    class Program
+    {
+        static strana az = new strana(); //Объявляем переменную *strana*
+
+        static void Main(string[] args)
+        {
+            for (int z = 0; z < 10000; z++)
+            { //Создаём меню программы
+                Console.Write("Введите номер операции которую вы хотите выполнить: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                switch (x)
+                {
+                    case 1: az.add(); break;
+                    case 2: az.print(); break;
+                    case 3: Console.Clear(); break;
+                    case 4: Environment.Exit(0); break;
+                    default: Console.WriteLine("Error! Incorrect input!"); break; //Проверка //на некорректный ввод данных, т.е при вводе числа >5 получаем ошибку
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
